ad9704ce30823c4e69eefb0d87d8edbb0dc2464e
joshlobo4289 <joshua.jude.lobo@gmail.com>
2021-03-06 10:09:54 +0000

    an example of how to use the process object in node

---
+++ 3-process/1-custom-env-variables.js
@@ -0,0 +1,12 @@
+console.log('Current user is', process.env.USER);
+
+console.log('\nScript executed with:');
+
+console.log('VAL1 equalt to:', process.env.VAL1);
+console.log('VAL2 equalt to:', process.env.VAL2);
+
+
+// using process instead 
+
+process.stdout.write("this is a line from the node process\n");
+process.stdout.("this is a line from the node process\n");
