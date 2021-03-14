f0ea5a49024a50cf25dbe865fa4308cbfe92ec25
joshlobo4289 <joshua.jude.lobo@gmail.com>
2021-03-14 20:36:12 +0000

    nmade point about node having a wrapping function

--- 6-modules/1-define-use/2-argument.js
+++ 6-modules/1-define-use/2-argument.js
@@ -1,3 +1,9 @@
+// node wraps arguments in a function 
+
+// In a browser context arguments is undefined 
+// but in the node envoironment it wraps in a function and returns a lot of info  
+
+
 function dynamicArgsFunction() {
   console.log(arguments);
 }
@@ -7,6 +13,7 @@ dynamicArgsFunction(3, 7, 5, 4);
 function dynFunc(){
   console.log("This is how js sees argv ");
   console.log(arguments);
-  
+
 }
 dynFunc(3,5,4,5,5,5);
+
